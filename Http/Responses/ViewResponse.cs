261d4a663041c96eba8e3449b373c10cba40f0e8
RadoPenev <radomir.penev16@gmail.com>
2024-10-03 19:31:57 +0300

    View templating added

--- Http/Responses/ViewResponse.cs
+++ Http/Responses/ViewResponse.cs
@@ -10,7 +10,7 @@ namespace Http.Responses
     public class ViewResponse: ContentResponse
     {
         private const char PathSeparator ='/';
-        public ViewResponse(string viewName, string controllerName) 
+        public ViewResponse(string viewName, string controllerName, object model=null) 
             : base(string.Empty, ContentType.Html)
         {
             if (!viewName.Contains(PathSeparator))
@@ -21,7 +21,33 @@ namespace Http.Responses
             var viewPath=Path.GetFullPath($"./Views/{viewName.TrimStart(PathSeparator)}.cshtml");
             var viewContent=File.ReadAllText(viewPath);
 
+            if (model!=null)
+            {
+                viewContent = PopulateModel(viewContent, model);
+            }
+
             Body=viewContent;
+        }
+
+        private string PopulateModel(string viewContent, object model)
+        {
+            var data = model
+                 .GetType()
+                 .GetProperties()
+                 .Select(p => new
+                 {
+                     p.Name,
+                     Value = p.GetValue(model)
+                 });
+
+            foreach (var item in data)
+            {
+                const string openingBrackets = "{{";
+                const string closingBrackets = "}}";
+               viewContent= viewContent.Replace($"{openingBrackets}{item.Name}{closingBrackets}", item.Value.ToString());
+            }
+
+            return viewContent;
         }
     }
 }
