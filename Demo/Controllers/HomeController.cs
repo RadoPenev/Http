82ed4829c599bd813833b30d0e8868fda42d74c3
RadoPenev <radomir.penev16@gmail.com>
2024-10-03 19:32:31 +0300

    HtmlForm action transformed to use view templating

--- Demo/Controllers/HomeController.cs
+++ Demo/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Http.Controllers;
+﻿using Demo.Models;
+using Http.Controllers;
 using Http.HTTP;
 using System.Text;
 using System.Web;
@@ -24,14 +25,16 @@ namespace Demo.Controllers
 
         public Response HtmlFormPost()
         {
-            StringBuilder sb = new StringBuilder();
+           string name=Request.Form["Name"];
+            string age = Request.Form["Age"];
 
-            foreach (var (key,value) in Request.Form)
+            var model = new FormViewModel()
             {
-                sb.AppendLine($"{key} - {value}");
-            }
+                Name=name,
+                Age=int.Parse(age)
+            };
 
-            return Text(sb.ToString());
+            return View(model);
         }
 
         
